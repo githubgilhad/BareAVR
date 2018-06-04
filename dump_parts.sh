c6437f34048a42bdb289c32c132d41de7bb7f079
gilhad <gilhad@seznam.cz>
2018-06-04 21:39:07 +0200

    Closed due MS acquisition, find me at https://divnoblog.wordpress.com/

--- dump_parts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# vim: fileencoding=utf-8:nomodified:nowrap:textwidth=0:foldmethod=marker:foldcolumn=4:syntax=sh:filetype=sh:ruler:showcmd:lcs=tab\:|- list
-
-avrdude -c usbasp -t -p atmega328p <<EOF
-part
-quit
-EOF
